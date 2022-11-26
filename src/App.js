ebe062d3db6b697d5badbea082f821dfe6324986
bdiddy2022 <brendan.t.downs@gmail.com>
2022-11-25 21:39:38 -0500

    successfully launched, ran, and edited with TailwindCSS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BiArchive } from "react-icons/bi"
 
 function App() {
   return (
-    <div className="App">
-      <h1><BiArchive />Your Appointments</h1>
+    <div className="App container mt-3 font-thin mx-auto">
+      <h1 className="text-5xl"><BiArchive className="inline-block text-red-400 align-top"/>Your Appointments</h1>
     </div>
   );
 }
