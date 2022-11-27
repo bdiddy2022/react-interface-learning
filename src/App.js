6f1319f70a2110a1bd6e51478f3d03b2053c9592
bdiddy2022 <brendan.t.downs@gmail.com>
2022-11-27 12:58:09 -0500

    used JS native 'filter' & 'includes' functions successfully

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import AppointmentInfo from "./components/AppointmentInfo";
 function App() {
   
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+  );
   
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -18,18 +29,20 @@ function App() {
 //"retrieving the data, and using useCallback() to monitor for any changes||additions to the data. this is how to work with a 'traditional' API"
 
   useEffect(() => {
-    fetchData()
-  }, [fetchData]);
+        fetchData()
+    }, [fetchData]
+  );
 
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+      onQueryChange={myQuery =>setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList.map(appointment => (
+        {filteredAppointments.map(appointment => (
           <AppointmentInfo key={appointment.id}
             appointment={appointment}
             onDeleteAppointment={
--- src/components/Search.js
+++ src/components/Search.js
@@ -31,31 +31,35 @@ const Dropdown = ({toggle}) => {
   }
 }
 
-const Search = () => {
+const Search = ({ query, onQueryChange }) => {
   let [toggleSort, setToggleSort] = useState(false);
-    return (
-        <div className="py-5">
-        <div className="mt-1 relative rounded-md shadow-sm">
+  
+  return (
+    <div className="py-5">
+      <div className="mt-1 relative rounded-md shadow-sm">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <BiSearch />
-            <label htmlFor="query" className="sr-only" />
+          <BiSearch />
+          <label htmlFor="query" className="sr-only" />
           </div>
-          <input type="text" name="query" id="query" value=""
-            className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
+          <input type="text" 
+            name="query" id="query" value={query}
+            onChange={(event) => {onQueryChange(event.target.value)}}
+            className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block sm:text-sm border-gray-300 w-4/5" placeholder="Search"
+          />
           <div className="absolute inset-y-0 right-0 flex items-center">
-            <div>
-              <button type="button" onClick={ () => 
-                { setToggleSort(!toggleSort) }
-              }
-                className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
-                Sort By <BiCaretDown className="ml-2" />
-              </button>
-              <Dropdown toggle={ toggleSort }/>
-            </div>
+           <div>
+           <button type="button" onClick={ () => 
+          { setToggleSort(!toggleSort) }
+          }
+          className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+           Sort By <BiCaretDown className="ml-2" />
+        </button>
+        <Dropdown toggle={ toggleSort }/>
+      </div>    
           </div>
         </div>
-      </div>
-    );
+    </div>    
+  );
 }
 
 export default Search;
