b38087b1b257b4aee2788d8dbc713d618d68dc4f
bdiddy2022 <brendan.t.downs@gmail.com>
2022-12-02 18:40:37 -0500

    Implemented add appointment functionality using useState and spread operators

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
-  
+
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
   let [sortBy, setSortBy] = useState("petName");
@@ -23,51 +23,53 @@ function App() {
       );
     }
   ).sort((a, b) => {
-    let order = (orderBy === 'asc') ? 1: -1;
+    let order = (orderBy === 'asc') ? 1 : -1;
     return (
       a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
         ? (-1 * order) : (1 * order)
     );
   });
-  
+
   const fetchData = useCallback(() => {
     fetch('./data.json')
-    .then(response => response.json())
-    .then (data => {
+      .then(response => response.json())
+      .then(data => {
         setAppointmentList(data)
-    })
+      })
   }, [/*any tracked variables go here*/]);
-//"retrieving the data, and using useCallback() to monitor for any changes||additions to the data. this is how to work with a 'traditional' API"
+  //"retrieving the data, and using useCallback() to monitor for any changes||additions to the data. this is how to work with a 'traditional' API"
 
   useEffect(() => {
-        fetchData()
-    }, [fetchData]
+    fetchData()
+  }, [fetchData]
   );
 
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
-      <AddAppointment />
+      <AddAppointment
+        onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
+        lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)} />
       <Search query={query}
-      onQueryChange={myQuery =>setQuery(myQuery)}
-      orderBy = {orderBy}
-      onOrderByChange = {mySort => setOrderBy(mySort)}
-      sortBy = {sortBy}
-      onSortByChange = {mySort => setSortBy(mySort)}
-        />
+        onQueryChange={myQuery => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange={mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}
+      />
 
       <ul className="divide-y divide-gray-200">
         {filteredAppointments.map(appointment => (
           <AppointmentInfo key={appointment.id}
             appointment={appointment}
             onDeleteAppointment={
-              appointmentId => 
-              setAppointmentList(appointmentList.filter(appointment => 
+              appointmentId =>
+                setAppointmentList(appointmentList.filter(appointment =>
                   appointment.id !== appointmentId))
             }
           />
-          ))
+        ))
         }
       </ul>
     </div>
